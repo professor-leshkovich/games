c5e878aff803972ea6853cca4cdb4964c76d8b55
professor-leshkovich <xuipro777228@gmail.com>
2026-05-07 21:59:22 +0300

    +4 и1

--- сапер.py
+++ сапер.py
@@ -113,7 +113,7 @@ class Minesweeper:
             self.flags.append([False] * self.width)
     
     def place_mines(self, first_x, first_y):
-        # Размещает мины, гарантируя, что первая клетка не будет миной
+        # Размещает мины с гарантом
         mines_placed = 0
         while mines_placed < self.mines:
             x = random.randint(0, self.width - 1)
